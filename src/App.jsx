bf2efce3f57957af0819087ccfeeae90c947ce48
Kaiser Fayaz <kasirmalik87@gmail.com>
2024-11-22 06:43:17 -0800

    Spotify clone

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import Display from "./components/Display"
+import Player from "./components/Player"
+import Sidebar from "./components/Sidebar"
+import { PlayerContext } from "./context/PlayerContext"
+import { useContext } from "react"
+
+
+const App = () => {
+    const {audioRef,track} = useContext(PlayerContext)
+    return(
+      <div className="min-h-screen bg-black">
+        <div className="h-[90%] flex">
+          <Sidebar/>
+          <Display/> 
+        </div>
+        <Player/>
+        <audio ref={audioRef} src={track.file} preload="auto"></audio>
+      </div>
+      
+    
+  )
+}
+
+
+
+export default App
